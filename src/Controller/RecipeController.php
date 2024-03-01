68716316910730c759caea394d18671259c15ae2
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-03-01 09:25:16 +0100

    feat:  basic displays for recipes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,14 +13,17 @@ class RecipeController extends AbstractController
 {
 
     #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findAll();
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
 
     
     #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
         //dd($request->attributes->get('slug'), $request->attributes->getInt('id'));
         //dd($slug, $id); les deux donnes le même résultat
@@ -33,10 +37,12 @@ class RecipeController extends AbstractController
         // return $this->json([
         //     'slug' => $slug
         // ]);
-
+        $recipe = $repository->find($id);
+        if($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }    
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id
+            'recipe' => $recipe
         ]);
     }
 }
